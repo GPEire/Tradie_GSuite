8b66302cea2dcbec82a03e4cd25423c7404b9520
GPEire <james.murray99@gmail.com>
2025-11-05 17:56:14 -0800

    fix: correct DatePicker import path

--- frontend/src/components/ScanningConfiguration.tsx
+++ frontend/src/components/ScanningConfiguration.tsx
@@ -26,7 +26,7 @@ import {
   Save as SaveIcon,
   Refresh as RefreshIcon,
 } from '@mui/icons-material';
-import { DatePicker } from '@mui/x-date-pickers';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { apiClient } from '../services/api';
